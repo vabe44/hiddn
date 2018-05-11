cf979e98f2c799062a1c0c9bff0bf5f8fe006356
Benedek Vass <vabe44@gmail.com>
2018-05-12 00:23:22 +0200

    as

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    authDomain: "fir-demo-78788.firebaseapp.com",
+    databaseURL: "https://fir-demo-78788.firebaseio.com",
+    projectId: "fir-demo-78788",
+    storageBucket: "fir-demo-78788.appspot.com",
+    messagingSenderId: "252401946361"
   }
 };
