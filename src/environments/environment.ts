4e36e58e09903f83d56e498bf48388cc23e29b54
Benedek Vass <vabe44@gmail.com>
2018-05-13 17:56:48 +0200

    added overlay to cards

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "fir-demo-78788.firebaseapp.com",
-    databaseURL: "https://fir-demo-78788.firebaseio.com",
-    projectId: "fir-demo-78788",
-    storageBucket: "fir-demo-78788.appspot.com",
-    messagingSenderId: "252401946361"
+    apiKey: '',
+    authDomain: 'fir-demo-78788.firebaseapp.com',
+    databaseURL: 'https://fir-demo-78788.firebaseio.com',
+    projectId: 'fir-demo-78788',
+    storageBucket: 'fir-demo-78788.appspot.com',
+    messagingSenderId: '252401946361'
   }
 };
